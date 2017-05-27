7c007cdbebd727fd7887e31e2695d94d5fc8efe9
Tim Vivian-Griffiths <tvivian-griffiths@mango-solutions.com>
2017-05-27 12:49:51 +0100

    Initial commit for the flask_blog from Udemy

---
+++ author/views.py
@@ -0,0 +1,13 @@
+from flask_blog import app
+from flask import render_template, redirect
+from author.form import RegisterForm
+
+@app.route('/login')
+def login():
+    return 'Hello User!'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    return render_template('author/register.html',
+                           form = form)
