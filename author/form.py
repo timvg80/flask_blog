cebb54d5886fb350f0ac591acbd8e5aaaa80b6a2
Tim Vivian-Griffiths <tvivian-griffiths@mango-solutions.com>
2017-06-20 14:31:18 +0100

    Fixing syntax and depreciations in author/form.py

--- author/form.py
+++ author/form.py
@@ -4,15 +4,15 @@ from wtforms.fields.html5 import EmailField
 
 
 class RegisterForm(Form):
-    fullname  = StringField('Full Name', [validators.Required()])
-    email     = EmailField('Email', [validators.Required()])
-    username  = StringField('Username', [
-        validators.Required(),
+    fullname = StringField('Full Name', [validators.DataRequired()])
+    email = EmailField('Email', [validators.DataRequired()])
+    username = StringField('Username', [
+        validators.DataRequired(),
         validators.Length(min=4, max=25)
     ])
-    password  = PasswordField('New Password', [
-        validators.Required(),
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords much match'),
         validators.Length(min=4, max=80)
     ])
-    confirm   = PasswordField('Repeat password')
+    confirm = PasswordField('Repeat password')
