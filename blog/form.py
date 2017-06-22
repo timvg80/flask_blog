12026dec50719dca4b6f3d5a870e74a43553edad
Tim Vivian-Griffiths <tvivian-griffiths@mango-solutions.com>
2017-06-22 16:00:44 +0100

    blog form views for admin and setup

--- blog/form.py
+++ blog/form.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, validators
 from author.form import RegisterForm
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
-from flask_blog import app
+from flask_blog import app, db
+from flask import render_template, redirect, flash, url_for
+from blog.form import SetupForm
+from author.models import Author
+from blog.models import Blog
+
+
+@app.route('/admin')
+def admin():
+    blogs = Blog.query.count()
+    if blogs == 0:
+        return redirect(url_for('setup'))
+    return render_template('blog/admin.html')
+
+
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+    form = SetupForm()
+    return render_template('blog/setup.html', form=form)
 
 
 @app.route('/')
