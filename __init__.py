20b1725b370b5c8b4657551db79fc062fdd9be81
Tim Vivian-Griffiths <tvivian-griffiths@mango-solutions.com>
2017-06-26 20:25:57 +0100

    Adding the flask migrate to init and manage

--- __init__.py
+++ __init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
+
+
+# Migration
+migrate = Migrate(app, db)
 
 from blog import views
 from author import views
--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 from flask_script import Manager, Server
+from flask_migrate import MigrateCommand
 from flask_blog import app
 
 manager = Manager(app)
 
+
+manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server(
     use_debugger=True,
     use_reloader=True)
